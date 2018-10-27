cc7d073870edcb8fb4ee984253d3a00d5f79ebb7
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-27 22:35:40 +0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
+  root to: 'events#index'
 end
