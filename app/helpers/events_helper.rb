4cef6cd05a3a507a44f9826b2cdb4712dfd194fd
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-28 06:21:00 +0500

    Formatted date

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   def render_date(start_date = nil, end_date = nil)
-    return [start_date, end_date].join(' to ') if start_date.present? && end_date.present?
-    start_date
+    return [start_date.strftime('%m/%d/%y'), end_date.strftime('%m/%d/%y')].join(' to ') if start_date.present? && end_date.present?
+    start_date.strftime('%m/%d/%y')
   end
 end
